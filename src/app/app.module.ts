6dcc231487410963f78559b45f6df245ce0f8a82
uriasvitor <uriasvitor@gmail.com>
2023-11-24 00:17:29 -0300

    add: style for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './shared/input/input.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
