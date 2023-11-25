d0060ecfe80ae43039d5248f54ac71c4aa2fad07
uriasvitor <uriasvitor@gmail.com>
2023-11-24 23:49:56 -0300

    Add: login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputComponent } from './shared/input/input.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { TabsComponent } from './tabs/tabs.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    TabsComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    RouterLink,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss']
+})
+export class TabsComponent {
+  selectedTab: string = '';
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+}
