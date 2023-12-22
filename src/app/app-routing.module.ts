627b3583ccb9b39b4887d6c2c6cec9213efa8bc1
uriasvitor <uriasvitor@gmail.com>
2023-12-22 14:22:28 -0300

    add: new logo to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { authGuard } from './services/auth/authGuard.service';
-import { SignInComponent } from './user/sign-in/sign-in.component';
-import { UploadComponent } from './image/upload/upload.component';
 
 const routes: Routes =[
   {
     path:'',
     component:HomeComponent,
-    canActivate:[authGuard]
-  },
-  {
-    path:'upload',
-    component:UploadComponent,
     canActivate:[authGuard]
   },
   {
